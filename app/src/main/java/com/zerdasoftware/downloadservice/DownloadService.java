9cdf455e20c2e13c3b872e6d42337222146794b8
cihancalli <cihan.callii@gmail.com>
2022-05-04 21:23:41 +0300

    Download Web Site

--- app/src/main/java/com/zerdasoftware/downloadservice/DownloadService.java
+++ app/src/main/java/com/zerdasoftware/downloadservice/DownloadService.java
@@ -7,6 +7,12 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 public class DownloadService extends Service {
     @Nullable
     @Override
@@ -16,6 +22,15 @@ public class DownloadService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        AsyncTaskClass asyncTaskClass = new AsyncTaskClass();
+        try {
+            asyncTaskClass.execute("https://cihancalli.com/").get();
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -23,13 +38,37 @@ public class DownloadService extends Service {
 
         @Override
         protected String doInBackground(String... strings) {
+            String result = "";
 
+            URL url;
+            HttpURLConnection httpURLConnection = null;
 
-            return null;
+            try {
+                url =  new URL(strings[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while (data != -1){
+                    char current = (char) data;
+                    result += current;
+
+                    data = inputStreamReader.read();
+                }
+
+                return result;
+
+            }catch (Exception e){
+                return "Failed";
+            }
         }
 
         @Override
         protected void onPostExecute(String s) {
+            System.out.println("Result: "+s);
             super.onPostExecute(s);
         }
     }
